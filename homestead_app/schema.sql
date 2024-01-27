e35dc4fdc9200ea3292202031a4f01d7b96fe7e9
void-ninja <cade.huelsman@gmail.com>
2024-01-27 09:16:40 -0500

    Added overflow-y to the log table
    Makes it so that the note and delete dialogs display at a reasonable spot instead of way down below the viewport

--- homestead_app/schema.sql
+++ homestead_app/schema.sql
@@ -6,7 +6,4 @@ CREATE TABLE eggs(
     notes TEXT
 );
 
-INSERT INTO eggs (collection_date, amount) VALUES ('0000/10/01',12);
-INSERT INTO eggs (collection_date, amount, notes) VALUES ('1000/11/02',20,'test notes');
--- INSERT INTO eggs (collection_date, amount) VALUES ('A little later (test)',3);
--- INSERT INTO eggs (collection_date, amount) VALUES ('Sometime (test)',0);
+--INSERT INTO eggs (collection_date, amount, notes) VALUES ('2024/01/24',12,'1 from second coop');
